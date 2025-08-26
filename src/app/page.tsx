200004a3d257953a9e79bedbb62ffbed3177c439
andersonheidrich <andersonheidrich@hotmail.com>
2025-08-26 17:25:41 -0300

    testando deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="flex w-full min-h-screen">Heidrich E-commerce</div>;
+  return <div className="flex w-full min-h-screen">E-commerce</div>;
 }
