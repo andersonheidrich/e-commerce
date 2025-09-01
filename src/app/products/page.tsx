67dd08633675f5a4e634c586e5175686e9c70a55
andersonheidrich <andersonheidrich@hotmail.com>
2025-09-01 20:34:08 -0300

    fix: corrigido carregamento do products/page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,14 @@
 import { ProductList } from "@/features/product/components";
+import { Suspense } from "react";
 
 export default function ProductsPage() {
   return (
     <div className="flex w-full min-h-screen justify-center items-center">
-      <ProductList />
+      <Suspense
+        fallback={<p className="text-center mt-20">Carregando produtos...</p>}
+      >
+        <ProductList />
+      </Suspense>
     </div>
   );
 }
